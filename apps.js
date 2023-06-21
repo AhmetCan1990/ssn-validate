2f83433b33ecf2b903f8a474b4de8ed821d1e979
Ahmet <ahmetcanaydin1990@gmail.com>
2023-06-21 23:57:26 +0300

    first commit

---
+++ apps.js
@@ -0,0 +1,35 @@
+const formSubmit = document.getElementById("formSubmit");
+const formInput = document.getElementById("formInput");
+const container = document.querySelector(".container");
+formSubmit.addEventListener("submit", (event) => {
+  event.preventDefault();
+  validation(formInput.value.trim().split("-"));
+});
+function displayAlert(alertMessage) {
+  const divAlert = document.createElement("div");
+  divAlert.setAttribute("class", "alert alert-danger");
+  divAlert.setAttribute("role", "alert");
+  divAlert.innerText = alertMessage;
+  container.append(divAlert);
+}
+function validation(ssnArr) {
+  if (
+    ssnArr.length == 3 &&
+    ssnArr[0].length == 3 &&
+    ssnArr[1].length == 2 &&
+    ssnArr[2].length == 4 &&
+    formInput.value.match(/[0-9]/gi).length == formInput.value.length - 2 &&
+    ssnArr[0] != "000" &&
+    ssnArr[0] != "666" &&
+    Number(ssnArr[0]) < 900 &&
+    ssnArr[1] != "00" &&
+    ssnArr[2] != "0000"
+  ) {
+    displayAlert(`Entered SSN (${formInput.value}) is valid`);
+  } else {
+    displayAlert(`SSN must be 11 characters (9 numbers and 2 hypens)`);
+  }
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 3000);
+}
